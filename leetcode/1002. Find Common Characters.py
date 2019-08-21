f4979b62bb8d8de13b668f3ae528d16fcc337fbf
wangzyh <1149061045@qq.com>
2019-08-21 20:23:17 +0800

    2019/8/21

---
+++ leetcode/1002. Find Common Characters.py
@@ -0,0 +1,8 @@
+# 1002. Find Common Characters
+def main(A):
+
+
+
+if __name__ == '__main__':
+    A = ["bella","label","roller"]
+    main(A)
---
+++ leetcode/155. Min Stack.py
@@ -0,0 +1,45 @@
+# region Min Stack
+"""
+Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
+
+push(x) -- Push element x onto stack.
+pop() -- Removes the element on top of the stack.
+top() -- Get the top element.
+getMin() -- Retrieve the minimum element in the stack.
+
+
+Example:
+
+MinStack minStack = new MinStack();
+minStack.push(-2);
+minStack.push(0);
+minStack.push(-3);
+minStack.getMin();   --> Returns -3.
+minStack.pop();
+minStack.top();      --> Returns 0.
+minStack.getMin();   --> Returns -2.
+"""
+# endregion
+
+
+class MinStack:
+
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+
+    def push(self, x: int) -> None:
+
+    def pop(self) -> None:
+
+    def top(self) -> int:
+
+    def getMin(self) -> int:
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(x)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
