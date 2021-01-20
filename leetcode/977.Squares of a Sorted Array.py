fe54c77c8a5e33707bd0eaf33b2845c53ac178f9
wangzyh <31379713+wangzyh@users.noreply.github.com>
2021-01-20 16:39:08 +0800

    977

---
+++ leetcode/977.Squares of a Sorted Array.py
@@ -0,0 +1,51 @@
+# Given an integer array nums sorted in non-decreasing order, return an array of
+#  the squares of each number sorted in non-decreasing order. 
+# 
+#  
+#  Example 1: 
+# 
+#  
+# Input: nums = [-4,-1,0,3,10]
+# Output: [0,1,9,16,100]
+# Explanation: After squaring, the array becomes [16,1,0,9,100].
+# After sorting, it becomes [0,1,9,16,100].
+#  
+# 
+#  Example 2: 
+# 
+#  
+# Input: nums = [-7,-3,2,3,11]
+# Output: [4,9,9,49,121]
+#  
+# 
+#  
+#  Constraints: 
+# 
+#  
+#  1 <= nums.length <= 104 
+#  -104 <= nums[i] <= 104 
+#  nums is sorted in non-decreasing order. 
+#  
+# 
+#  
+# Follow up: Squaring each element and sorting the new array is very trivial, co
+# uld you find an O(n) solution using a different approach? Related Topics Array T
+# wo Pointers 
+#  👍 1926 👎 111
+
+# region data
+# 2021-01-20 16:33:30
+# endregion
+
+# leetcode submit region begin(Prohibit modification and deletion)
+class Solution:
+    def sortedSquares(self, nums):
+        return sorted(list(map(lambda x: x ** 2, nums)))
+
+
+# leetcode submit region end(Prohibit modification and deletion)
+
+
+if __name__ == '__main__':
+    n = [-4, -1, 0, 3, 10]
+    print(Solution().sortedSquares(n))
